83676c70742333b93c607d73630a66078e4d44f2
Manuel Silva <82286102+manuelcsilva@users.noreply.github.com>
2023-12-27 14:26:11 +0000

    Atualização main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def on_logo_pressed():
     global num
     num = 0
     kitronik_VIEW128x64.clear()
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + str(num)),
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convert_to_text(num),
         1,
         kitronik_VIEW128x64.ShowAlign.CENTRE)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
@@ -10,7 +10,8 @@ input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 def on_button_pressed_a():
     global num
     num = num + 1
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + str(num)),
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convert_to_text(num),
         1,
         kitronik_VIEW128x64.ShowAlign.CENTRE)
     kitronik_VIEW128x64.show(Math.sqrt(num), 2)
@@ -20,7 +21,8 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_b():
     global num
     num = num - 1
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + str(num)),
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convert_to_text(num),
         1,
         kitronik_VIEW128x64.ShowAlign.CENTRE)
     kitronik_VIEW128x64.show(Math.sqrt(num), 2)
@@ -29,8 +31,21 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 num = 0
 kitronik_VIEW128x64.control_display_on_off(kitronik_VIEW128x64.on_off(True))
-kitronik_VIEW128x64.set_font_size(kitronik_VIEW128x64.FontSelection.NORMAL)
 serial.write_line("ON")
-kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + str(num)),
+kitronik_VIEW128x64.show("OLED01",
+    1,
+    kitronik_VIEW128x64.ShowAlign.CENTRE,
+    kitronik_VIEW128x64.FontSelection.BIG)
+kitronik_VIEW128x64.show("Raiz",
+    2,
+    kitronik_VIEW128x64.ShowAlign.CENTRE,
+    kitronik_VIEW128x64.FontSelection.BIG)
+kitronik_VIEW128x64.show("Quadrada",
+    3,
+    kitronik_VIEW128x64.ShowAlign.CENTRE,
+    kitronik_VIEW128x64.FontSelection.BIG)
+basic.pause(2000)
+kitronik_VIEW128x64.clear()
+kitronik_VIEW128x64.show("Raiz Quadrada de " + convert_to_text(num),
     1,
     kitronik_VIEW128x64.ShowAlign.CENTRE)
--- main.ts
+++ main.ts
@@ -2,24 +2,30 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     
     num = 0
     kitronik_VIEW128x64.clear()
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + ("" + num)), 1, kitronik_VIEW128x64.ShowAlign.Centre)
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convertToText(num), 1, kitronik_VIEW128x64.ShowAlign.Centre)
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     num = num + 1
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + ("" + num)), 1, kitronik_VIEW128x64.ShowAlign.Centre)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convertToText(num), 1, kitronik_VIEW128x64.ShowAlign.Centre)
     kitronik_VIEW128x64.show(Math.sqrt(num), 2)
     serial.writeNumber(Math.sqrt(num))
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     num = num - 1
-    kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + ("" + num)), 1, kitronik_VIEW128x64.ShowAlign.Centre)
+    kitronik_VIEW128x64.clear()
+    kitronik_VIEW128x64.show("Raiz Quadrada de " + convertToText(num), 1, kitronik_VIEW128x64.ShowAlign.Centre)
     kitronik_VIEW128x64.show(Math.sqrt(num), 2)
     serial.writeNumber(Math.sqrt(num))
 })
 let num = 0
 kitronik_VIEW128x64.controlDisplayOnOff(kitronik_VIEW128x64.onOff(true))
-kitronik_VIEW128x64.setFontSize(kitronik_VIEW128x64.FontSelection.Normal)
 serial.writeLine("ON")
-kitronik_VIEW128x64.show("Raiz Quadrada de " + ("" + ("" + num)), 1, kitronik_VIEW128x64.ShowAlign.Centre)
+kitronik_VIEW128x64.show("OLED01", 1, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
+kitronik_VIEW128x64.show("Raiz", 2, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
+kitronik_VIEW128x64.show("Quadrada", 3, kitronik_VIEW128x64.ShowAlign.Centre, kitronik_VIEW128x64.FontSelection.Big)
+basic.pause(2000)
+kitronik_VIEW128x64.clear()
+kitronik_VIEW128x64.show("Raiz Quadrada de " + convertToText(num), 1, kitronik_VIEW128x64.ShowAlign.Centre)
